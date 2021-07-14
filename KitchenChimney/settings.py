0b3258d9c62098600c695697ad81a7de9237e357
arshadarif <64719078+arshadarif@users.noreply.github.com>
2021-07-14 16:53:52 +0530

    Update settings.py

--- KitchenChimney/settings.py
+++ KitchenChimney/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -140,6 +139,3 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [STATIC_DIR]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
